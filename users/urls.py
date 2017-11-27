81abb3f005882560c5a9b63819ab20f0b2337ee4
ChristianMae <Christianmae041997@gmail.com>
2017-11-27 13:27:54 +0800

    Fix merge conflict

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.conf.urls import url, include
-
 from .views import FacebookLogin, GoogleLogin
+from django.conf.urls import url
+from .views import UserProfile
+
 
 urlpatterns = [
     url(r'^accounts/', include('allauth.urls')),
     url(r'^api/auth/', include('rest_auth.urls')),
     url(r'^api/auth/registration/', include('rest_auth.registration.urls')),
     url(r'^api/auth/facebook', FacebookLogin.as_view(), name='fb_login'),
-    url(r'^api/auth/google', GoogleLogin.as_view(), name='google_login')
+    url(r'^api/auth/google', GoogleLogin.as_view(), name='google_login'),
+    url(r'^user_profile/', UserProfile.as_view({'get': 'get', 'put': 'update'})),
 ]
--- users/views.py
+++ users/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
+from django.contrib.auth import get_user_model
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+from .serializers import UserDetailsSerializer
+
+User = get_user_model()
 
 
 class FacebookLogin(SocialLoginView):
@@ -10,3 +18,19 @@ class FacebookLogin(SocialLoginView):
 
 class GoogleLogin(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
+
+
+class UserProfile(ViewSet):
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UserDetailsSerializer
+
+    def get(self, request, *args, **kwargs):
+        serializer = UserDetailsSerializer(self.request.user)
+        return Response(serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        serializer = UserDetailsSerializer(self.request.user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_200_OK)
+        return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
