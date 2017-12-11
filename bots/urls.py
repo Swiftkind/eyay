50fdaebfafff60872adc6a0448f840f4632da6a3
sbenemerito <benemeritosam@gmail.com>
2017-12-11 16:55:19 +0800

    fixed permissions and added UserIsOwnerMixin

--- bots/urls.py
+++ bots/urls.py
@@ -1,25 +1,49 @@
 from django.conf.urls import url
 
 from .views import (
-    ArchiveListView,
-    ArchiveRUDView,
-    BotListCreateView, 
-    BotRUDView,
+    ArchiveViewSet,
+    BotViewSet,
     ChatBot,
-    KnowledgeListCreateView,
-    KnowledgeRUDView,
+    KnowledgeViewSet
     )
 
 
 urlpatterns = [
-    url(r'^api/bots/$', BotListCreateView.as_view(), name='bot-list'),
-    url(r'^api/bots/(?P<pk>[0-9]+)/$', BotRUDView.as_view(), name='bot-detail'),
-    url(r'^api/bots/(?P<pk>[0-9]+)/chat/$', ChatBot.as_view(), name='chat-with-bot'),
-    url(r'^api/bots/archive/$', ArchiveListView.as_view(), name='bots-archive-list'),
-    url(r'^api/bots/archive/(?P<pk>[0-9]+)/$', ArchiveRUDView.as_view(), 
-        name='bot-archive-detail'),
-    url(r'^api/bots/(?P<pk>[0-9]+)/knowledges/$',
-        KnowledgeListCreateView.as_view(), name='knowledges-api'),
+    url(r'^api/bots/archive/$', 
+        ArchiveViewSet.as_view({'get': 'list'}), 
+        name='bots-archive-list'
+        ),
+    url(r'^api/bots/archive/(?P<pk>[0-9]+)/$', 
+        ArchiveViewSet.as_view({
+            'get': 'retrieve': 'put': 'update', 
+            'patch': 'partial_update', 'delete': 'destroy'
+            }), 
+        name='bot-archive-detail'
+        ),
+    url(r'^api/bots/$', 
+        BotViewSet.as_view({'get': 'list', 'post': 'create'}), 
+        name='bot-list'
+        ),
+    url(r'^api/bots/(?P<pk>[0-9]+)/$', 
+        BotViewSet.as_view({
+            'get': 'retrieve': 'put': 'update', 
+            'patch': 'partial_update', 'delete': 'destroy'
+            }), 
+        name='bot-detail'
+        ),
+    url(r'^api/bots/(?P<pk>[0-9]+)/chat/$', 
+        ChatBot.as_view(), 
+        name='chat-with-bot'
+        ),
+    url(r'^api/bots/(?P<bot>[0-9]+)/knowledges/$',
+        KnowledgeViewSet.as_view({'get': 'list', 'post': 'create'}), 
+        name='knowledges-api'
+        ),
     url(r'^api/bots/(?P<bot>[0-9]+)/knowledges/(?P<pk>[0-9]+)/$',
-        KnowledgeRUDView.as_view(), name='knowledge-detail'),
+        KnowledgeViewSet.as_view({
+            'get': 'retrieve': 'put': 'update', 
+            'patch': 'partial_update', 'delete': 'destroy'
+            }),
+        name='knowledge-detail'
+        ),
 ]
