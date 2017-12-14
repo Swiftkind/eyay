53eef1697dcd09aef958a0854049d3c9bee594c7
sbenemerito <benemeritosam@gmail.com>
2017-12-14 16:12:25 +0800

    filtering and pagination, pages for user bots and archive

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,20 @@
 from django.conf.urls import url, include
-from .views import FacebookLogin, GoogleLogin, UserProfileViewSet, UserProfile
+from django.contrib.auth.views import logout as logout_view
+from .views import (
+    FacebookLogin, 
+    GoogleLogin, 
+    UserProfileViewSet, 
+    UserProfile
+    )
 
 
 urlpatterns = [
+    url(r'^accounts/logout/$', logout_view, {'next_page': '/'}),
+    url(r'^accounts/profile', UserProfile.as_view(), name='user_profile'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^api/auth/', include('rest_auth.urls')),
     url(r'^api/auth/registration/', include('rest_auth.registration.urls')),
     url(r'^api/auth/facebook', FacebookLogin.as_view(), name='fb_login'),
     url(r'^api/auth/google', GoogleLogin.as_view(), name='google_login'),
     url(r'^user_profile/', UserProfileViewSet.as_view({'get': 'get', 'put': 'update'})),
-    url(r'^accounts/profile', UserProfile.as_view(), name='user_profile'),
 ]
