67c2903309b8e85274c76b7d3a6b251f255f76aa
sbenemerito <benemeritosam@gmail.com>
2017-12-14 16:16:53 +0800

    minor fix, sm to lg

--- bots/utils.py
+++ bots/utils.py
@@ -3,7 +3,7 @@ import string
 import spacy
 
 
-parse_text = spacy.load('en_core_web_sm')
+parse_text = spacy.load('en_core_web_lg')
 
 def clean_text(text):     
     text = ' '.join([word for word in text.strip().lower().split()])
